d11327c31344466b6e3794934e1dcad956470e91
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-04-11 22:51:45 +0000

    Added frontend

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-navbar style="background-color:#990033">
+        <template #end>
+            <b-navbar-item href="#" class="has-text-grey-light">
+                Movies
+            </b-navbar-item>
+            <b-navbar-item href="#" class="has-text-grey-light">
+                Actors
+            </b-navbar-item>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-light" @click="logout">
+                        Log out
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+    export default {
+        name: 'Movie',
+        components: {
+        },
+        methods: {
+            // Log the user out
+            logout() {
+                this.$auth.logout({
+                    returnTo: window.location.origin
+                });
+            }
+        }
+    }
+</script>
---
+++ frontend/src/views/Movie.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <NavBar/>
+        <div class="container">
+            <div class="pt-4"><b-button type="is-primary" @click="login" style="background-color:#990033; float:right">Add Movie</b-button></div>
+            <div class="center" v-if="!movies.length">
+                <img alt="Vue logo" src="../assets/movies.png" style="height:120px"/>
+                <p style="color:#990033" class="has-text-weight-bold is-capitalized is-size-4">No Movies</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import NavBar from "../components/NavBar";
+
+    export default {
+        name: 'Movie',
+        components: {
+            NavBar
+        },
+        data(){
+            return {
+                movies: []
+            }
+        },
+        methods: {
+            // Log the user out
+            logout() {
+                this.$auth.logout({
+                    returnTo: window.location.origin
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.center {
+    padding: 150px 0;
+}
+</style>
