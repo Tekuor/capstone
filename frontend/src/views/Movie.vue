2d3dbcc4c0e76eee547249f23101f7899e7fd001
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-04-12 00:39:26 +0000

    modified text

--- frontend/src/views/Movie.vue
+++ frontend/src/views/Movie.vue
@@ -5,7 +5,7 @@
             <div class="pt-4"><b-button type="is-primary" @click="login" style="background-color:#990033; float:right">Add Movie</b-button></div>
             <div class="center" v-if="!movies.length">
                 <img alt="Vue logo" src="../assets/movies.png" style="height:120px"/>
-                <p style="color:#990033" class="has-text-weight-bold is-capitalized is-size-4">No Movies</p>
+                <p style="color:#990033" class="has-text-weight-bold is-capitalized is-size-4">No Movies Available</p>
             </div>
         </div>
     </div>
