40402dd76f4d5eef9a2c00fdcd12c75bbb415123
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-04-13 22:18:11 +0000

    added route for add movie

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Movie from "../views/Movie.vue";
+import AddMovie from "../views/AddMovie.vue";
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/movies',
     name: 'Movie',
     component: Movie
+  },
+  {
+    path: '/add-movie',
+    name: 'AddMovie',
+    component: AddMovie
   },
   {
     path: '/about',
