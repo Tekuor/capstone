01a72e2766b092c8e550ce4b54ba6addd64a4f70
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-04-17 00:04:22 +0000

    added environment variables

---
+++ heroku_sample/starter/setup.sh
@@ -0,0 +1,9 @@
+export DATABASE_URL='postgresql://postgres:123456@localhost:5432/agency'
+export AUTH0_DOMAIN='kuor.us.auth0.com'
+export ALGORITHMS='RS256'
+export API_AUDIENCE='agency'
+
+# Tokens
+export ASSISTANT='token_value_1'
+export DIRECTOR='token_value_2'
+export PRODUCER='token_value_3'
