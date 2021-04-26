9d5b6c54f85b6acba2753ff0bab0e11d28bc072c
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-04-26 01:57:58 +0000

    modified DATABASE_URL

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgresql://postgres:123456@localhost:5432/agency'
+export DATABASE_URL='postgres://postgres:123456@localhost:5432/agency'
 export AUTH0_DOMAIN='kuor.us.auth0.com'
 export ALGORITHMS='RS256'
 export API_AUDIENCE='agency'
